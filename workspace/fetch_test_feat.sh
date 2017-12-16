b0bd9f1c537f02a89703d4e30a540458005e07de
mingzhew <mzwang@umich.edu>
2017-12-15 22:31:21 -0500

    Update fetch_test_feat.sh
    update url

--- workspace/fetch_test_feat.sh
+++ workspace/fetch_test_feat.sh
@@ -1,3 +1,4 @@
-wget https://googledrive.com/host/0B-w3Rmi68HZeaFNaZ19WTkR1U1E -O test.tar.gz
+curl -c /tmp/cookies "https://drive.google.com/uc?export=download&id=0B-w3Rmi68HZeaFNaZ19WTkR1U1E" > /tmp/intermezzo.html
+curl -L -b /tmp/cookies "https://drive.google.com$(cat /tmp/intermezzo.html | grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | sed 's/\&amp;/\&/g')" > test.tar.gz
 tar -xvf test.tar.gz
 rm -rf test.tar.gz
