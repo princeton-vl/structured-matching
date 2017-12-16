6b510b3800ff6549e5a60c712b820bbad86d0ec2
mingzhew <mzwang@umich.edu>
2017-12-15 22:30:55 -0500

    Update fetch_model.sh
    update url

--- workspace/fetch_model.sh
+++ workspace/fetch_model.sh
@@ -1,3 +1,4 @@
-wget https://googledrive.com/host/0B-w3Rmi68HZeMmRnU1ZJdnJDYkk -O model.tar.gz
+curl -c /tmp/cookies "https://drive.google.com/uc?export=download&id=0B-w3Rmi68HZeMmRnU1ZJdnJDYkk" > /tmp/intermezzo.html
+curl -L -b /tmp/cookies "https://drive.google.com$(cat /tmp/intermezzo.html | grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | sed 's/\&amp;/\&/g')" > model.tar.gz
 tar -xvf model.tar.gz
 rm -rf model.tar.gz
