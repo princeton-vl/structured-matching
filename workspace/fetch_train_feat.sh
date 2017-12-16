68ff4c13910bda0042228d0a128ec8efe0be129d
mingzhew <mzwang@umich.edu>
2017-12-15 22:31:53 -0500

    Update fetch_train_feat.sh
    update url

--- workspace/fetch_train_feat.sh
+++ workspace/fetch_train_feat.sh
@@ -1,3 +1,4 @@
-wget https://googledrive.com/host/0B-w3Rmi68HZeRk1xU0N6dWFzdFU -O train.tar.gz
+curl -c /tmp/cookies "https://drive.google.com/uc?export=download&id=0B-w3Rmi68HZeRk1xU0N6dWFzdFU" > /tmp/intermezzo.html
+curl -L -b /tmp/cookies "https://drive.google.com$(cat /tmp/intermezzo.html | grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | sed 's/\&amp;/\&/g')" > train.tar.gz
 tar -xvf train.tar.gz
 rm -rf train.tar.gz
